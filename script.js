c6fe4ee1ec82449badbfa3d5a9022a1eec87f527
drmacze <drma801@gmail.com>
2025-07-02 00:40:13 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function login(provider) {
+  alert('Logging in with ' + provider + '...');
+  // Implement login logic here
+}
+function guestLogin() {
+  alert('Guest login started. Valid for 1 day.');
+  // Save session expiration in localStorage
+}
